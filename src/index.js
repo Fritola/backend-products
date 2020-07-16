09e853a6873655fefdaf0d4da38838d04abe9ab2
Fritola <fritola23@gmail.com>
2020-07-16 19:41:01 -0300

    environment var

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000 , () => {
+app.listen(process.env.PORT || 3000 , () => {
     console.log("Server running at 3000")
 })
 
